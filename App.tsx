2e33156df51c60aa6860580f128eadb1d3b38934
tanmayrane18 <tanmayrane51@gmail.com>
2025-07-28 11:39:43 +0530

    added the swipe animation like gmail

--- App.tsx
+++ App.tsx
@@ -8,14 +8,17 @@ import { View } from 'react-native';
 import './global.css';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Practise from './src/components/common/Practise';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
   return (
-    <GestureHandlerRootView className=" flex-1">
-      <View className=" flex-1 bg-white">
-        <Practise />
-      </View>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView className=" flex-1">
+        <View className=" flex-1 bg-white">
+          <Practise />
+        </View>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
