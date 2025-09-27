60cc06aeddabf570b853b7ad09e54205c78ad19f
tanmayrane18 <tanmayrane51@gmail.com>
2025-09-27 12:50:27 +0530

    splashscreen added

--- App.tsx
+++ App.tsx
@@ -3,7 +3,20 @@ import './global.css';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Practise from './src/components/common/Practise';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useEffect } from 'react';
+import BootSplash from "react-native-bootsplash";
+
 function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView className=" flex-1">
