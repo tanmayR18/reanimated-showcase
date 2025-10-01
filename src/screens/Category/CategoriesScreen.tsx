37709741edf2a4b8d8904d5d03540b9c3621e4ed
tanmayrane18 <tanmayrane51@gmail.com>
2025-10-01 23:45:17 +0530

    added categories to the home page

--- src/screens/Category/CategoriesScreen.tsx
+++ src/screens/Category/CategoriesScreen.tsx
@@ -1,21 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import React from 'react';
+import { categories } from '../../data/categories';
 
-type Props = {}
+const { height, width } = Dimensions.get('window');
 
-const CategoriesScreen = (props: Props) => {
-
-    const catgories = [
-        {
-            name: ''
-        }
-    ]
-
+const CategoriesScreen = () => {
   return (
-    <View>
-      <Text>CategoriesScreen</Text>
+    <View style={styles.container}>
+      {categories?.map(category => (
+        <View style={[styles.category, { backgroundColor: category.color}]} key={category.id}>
+          <Text style={styles.title}>{category.title}</Text>
+        </View>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default CategoriesScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 16,
+    gap: 8,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#BDBDBD'
+  },
+  category: {
+    width: width * 0.5 - 16 - 4,
+    height: width * 0.5,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    textAlign:'center',
+    color: '#555555'
+  },
+});
+
+export default CategoriesScreen;
